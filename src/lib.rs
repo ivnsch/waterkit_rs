58c55b01e56d693fcad27fc69a813908c3b142a9
Ivan Schuetz <ivanschuetz@gmail.com>
2025-01-18 14:42:35 +0100

    Add python interface function to get atoms, hydrogen bonds and rotatable bonds from python
    These are instantiated only once so ok to do it in python performance wise. SMARTs matching requires using openbabel in rust which adds complexity and little benefit over using openbabel in python.

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,6 @@ mod force_fields_parser;
 mod hydrogen_bonds;
 mod map_parser;
 mod params_parser;
-mod rotatable_bonds;
 mod run_waterkit;
 mod sampling;
 mod utils;
@@ -13,11 +12,48 @@ mod water;
 mod waterbox;
 pub mod waterkit;
 
+#[pyfunction]
+fn process_atom(input: (i32, String, String, i32, String, [f32; 3], f32, String)) -> PyResult<()> {
+    println!("got an atom from python!: {:?}", input);
+    Ok(())
+}
+
+#[pyfunction]
+fn process_atom_list(
+    input: Vec<(i32, String, String, i32, String, [f32; 3], f32, String)>,
+) -> PyResult<()> {
+    println!("got atom list from python!: {:?}", input);
+    Ok(())
+}
+
+#[pyfunction]
+fn process_hydrogen_bonds(input: Vec<(i32, [f32; 3], String, String)>) -> PyResult<()> {
+    println!("got hydrogen bond list from python!: {:?}", input);
+    Ok(())
+}
+
+#[pyfunction]
+fn process_rotatable_bonds(
+    input: Vec<(
+        i32,
+        i32,
+        [f32; 3],
+        [f32; 3],
+        [f32; 3],
+        [f32; 3],
+        Vec<i32>,
+        String,
+    )>,
+) -> PyResult<()> {
+    println!("got rotatable bond list from python!: {:?}", input);
+    Ok(())
+}
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
-use pyo3::prelude::*;
+use pyo3::{prelude::*, types::PyTuple};
 
 #[pyfunction]
 fn hello_rust(name: &str) -> PyResult<String> {
@@ -37,7 +73,10 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 fn string_sum(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     m.add_function(wrap_pyfunction!(hello_rust, m)?)?;
-    // m.add_function(wrap_pyfunction!(hydrate_rust, m)?)?;
+    m.add_function(wrap_pyfunction!(process_atom, m)?)?;
+    m.add_function(wrap_pyfunction!(process_atom_list, m)?)?;
+    m.add_function(wrap_pyfunction!(process_hydrogen_bonds, m)?)?;
+    m.add_function(wrap_pyfunction!(process_rotatable_bonds, m)?)?;
     Ok(())
 }
 
